5fd5dfaeb4d7ed670c59ef69c3d08fddd2a5654b
shams git <shamskhurasani481@gmail.com>
2021-07-24 00:22:28 +0200

    made the name part of the URL

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -12,39 +12,23 @@ class ConferenceController extends AbstractController
     // The main responsibility of a controller is to return an HTTP Response for the request.
     /**
      * @Route("/", name="homepage")
+     * @Route("/hello/{name}", name="homepage")
      */
-    public function index(Request $request): Response
-    {
-        // return $this->render('conference/index.html.twig', [
-        //     'controller_name' => 'ConferenceController',
-        // ]);
-        
-        // To generate a new response
-        //     return new Response(
-        //         // EOF => End of file
-        //         <<<EOF
-        //         <html>
-        //             <body>
-        //                 <img src="/img/construction.gif" style="display:block; margin:auto; margin-top: 15%"/>
-        //             </body>
-        //         </html>
-        //     EOF
-        // );
-
-        // To demonstrate how a response can leverage information from the request
-            $greet = '';
-            if($name = $request->query->get('hello')){
-                $greet = sprintf('<h1> Hello %s!</h1>', htmlspecialchars($name));
-            }
-            return new Response(
-                    <<<EOF
-                        <html>
-                            <body>
-                                $greet
-                                <img src="/img/construction.gif" style="display:block; margin:auto; margin-top: 15%"/>
-                            </body>
-                        </html>
-                    EOF
-                );
+    public function index(string $name=''){
+        // made the name part of the URL example: https://localhost:8000/hello/name
+        $greet = '';
+        if($name){
+            $greet = sprintf('<h1> Hello %s!</h1>', htmlspecialchars($name));
+        }
+        return new Response(
+            <<<EOF
+                <html>
+                    <body>
+                        $greet
+                        <img src="/img/construction.gif" style="display:block; margin:auto; margin-top: 15%"/>
+                    </body>
+                </html>
+            EOF
+        );
     }
 }
